8c8a530445f6e902609fe2b70b6ad70fc7225a35
ODO <dominikodo@onet.pl>
2023-04-13 22:32:54 +0200

    Stworzenie pobieranie z pliku punktów do obliczenia wielominu interpolacyjnego

--- Interpolacja/src/com/odo/zadania/mob/Interpolacja.java
+++ Interpolacja/src/com/odo/zadania/mob/Interpolacja.java
@@ -1,6 +1,76 @@
 package com.odo.zadania.mob;
 
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Interpolacja {
 //test
+
+    private ArrayList<Point> listaPunktow;
+
+    Interpolacja(){
+
+        listaPunktow=new ArrayList<>();
+
+        /*
+        Point punkt = new Point(1,2);
+
+        Point punkt2 = new Point(2,3);
+
+        Point punkt3 = new Point(3,4);
+
+        listaPunktow.add(punkt);
+
+        listaPunktow.add(punkt2);
+        listaPunktow.add(punkt3);
+
+       // System.out.println(listaPunktow);
+       */
+
+    }
+
+    void odczytZPliku (String nazwaPliku){
+
+        File file = new File(nazwaPliku);//tworzenie obiektu file
+        //System.out.println("test");
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+
+                Point punkt=new Point();
+
+                System.out.println("test");
+
+                punkt.setLocation(scanner.nextDouble(), scanner.nextDouble());
+                listaPunktow.add(punkt);
+
+                //listaPunktow.add(Double.parseDouble(scanner.nextLine()));
+               // System.out.println(punkt);
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Nie ma takiego pliku lub nie da się otworzyć");
+            System.exit(0);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Dane w pliku nie są liczbami");
+            System.exit(0);
+        }
+        System.out.println(listaPunktow);
+    }
+
+    String obliczWzor(){
+
+        String wielomian="test wiel";
+
+        return wielomian;
+    }
 
 }
--- Interpolacja/src/com/odo/zadania/mob/InterpolacjaTest.java
+++ Interpolacja/src/com/odo/zadania/mob/InterpolacjaTest.java
@@ -1,8 +1,36 @@
 package com.odo.zadania.mob;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class InterpolacjaTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean wylacz = false;
+        while (wylacz==false) {
+
+            Interpolacja interpolacja = new Interpolacja();
+            Scanner scanner = new Scanner(System.in);
+
+
+            //ArrayList<Point> p = new ArrayList<>();
+            String nazwaPliku = new String();
+
+            System.out.println("Podaj nazwe ścieżkę pliku: ");
+
+            nazwaPliku = scanner.nextLine();
+
+
+            interpolacja.odczytZPliku(nazwaPliku);
+
+            System.out.println(interpolacja.obliczWzor());
+
+
+            //C:\Users\piese\Desktop\Metody Obliczeniowe\Oblicznie_Wielomianu_Interpolacyjnego\Interpolacja\plik.txt
+        }
+
     }
+
 }
